f833d5354425db6b150f62ec3d32c8e41e2f76be
Adhikansh Mittal <adhikansh.mittal@bajajfinserv.in>
2021-06-27 12:11:24 +0530

    backend deployed

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getStates = async () => {
   const res = await axios.get(BASE_URL + "/states");
