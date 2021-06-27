9e8dfe822d23d616ae566f5974df0e0fcaf613c0
Adhikansh Mittal <adhikansh.mittal@bajajfinserv.in>
2021-06-27 14:55:57 +0530

    add api key

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,28 +1,33 @@
 import axios from "axios";
+import { convertDataIntoJson } from "../root/conversion";
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const getStates = async () => {
   const res = await axios.get(BASE_URL + "/states");
-  return res.data.data;
+  const _res = convertDataIntoJson(res.data.data);
+  return _res.data;
 };
 
 export const getDistricts = async (district) => {
   const res = await axios.get(
     BASE_URL + `/districts?districtId=${district.state_id}`
   );
-  return res.data.data;
+  const _res = convertDataIntoJson(res.data.data);
+  return _res.data;
 };
 
 export const getSlotsByDistrict = async (district, date) => {
   const res = await axios.get(
     BASE_URL + `/calendarByDistrict?districtId=${district}&date=${date}`
   );
-  return res.data;
+  const _res = convertDataIntoJson(res.data.data);
+  return _res;
 };
 
 export const getSlotsByPincode = async (pincode, date) => {
   const res = await axios.get(
     BASE_URL + `/calendarByPin?pincode=${pincode}&date=${date}`
   );
-  return res.data;
+  const _res = convertDataIntoJson(res.data.data);
+  return _res;
 };
