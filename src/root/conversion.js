e387f6c8111475b71e876bc1c1f002dbaa4bb368
Adhikansh Mittal <adhikansh.mittal@bajajfinserv.in>
2021-06-27 14:58:45 +0530

    remove consols

--- src/root/conversion.js
+++ src/root/conversion.js
@@ -2,6 +2,5 @@ import CryptoJS from "crypto-js";
 
 export const convertDataIntoJson = (data) => {
   const _decrypted_data = CryptoJS.AES.decrypt(data, process.env.REACT_APP_KEY);
-  console.log(_decrypted_data.toString(CryptoJS.enc.Utf8));
   return JSON.parse(_decrypted_data.toString(CryptoJS.enc.Utf8));
 };
